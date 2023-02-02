d89e835d666b768eaea2a09d0381346f6321abce
DeadlyLemonate <pongpop.suw@gmail.com>
2023-02-02 16:51:07 +0700

    change animate

--- components/listmenu.vue
+++ components/listmenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="width: 100%">
     <v-list nav dense>
       <v-list-item
         class="select_menu"
@@ -8,7 +8,16 @@
         @click="goto(item.linkss)"
       >
         <v-list-item-title class="unselect">
-          {{ item.menu }}
+          <div
+            style="
+              font-size: 46px;
+              line-height: 5rem;
+              text-align: center;
+              width: 100%;
+            "
+          >
+            {{ item.menu }}
+          </div>
         </v-list-item-title>
       </v-list-item>
     </v-list>
@@ -35,8 +44,97 @@ export default {
   },
 }
 </script>
+<style>
+.theme--light.v-navigation-drawer {
+  background: rgba(0, 0, 0, 0.75);
+}
+.v-navigation-drawer__content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.v-list--dense .v-list-item {
+  min-height: 80px;
+}
+.v-navigation-drawer {
+  transform: unset !important;
+  -webkit-animation: fadeInFromNone 1.5s ease-in-out;
+  -moz-animation: fadeInFromNone 1.5s ease-in-out;
+  -o-animation: fadeInFromNone 1.5s ease-in-out;
+  animation: fadeInFromNone 1.5s ease-in-out;
+}
+
+@-webkit-keyframes fadeInFromNone {
+  0% {
+    display: none;
+    opacity: 0;
+  }
+
+  1% {
+    display: block;
+    opacity: 0;
+  }
+
+  100% {
+    display: block;
+    opacity: 1;
+  }
+}
+
+@-moz-keyframes fadeInFromNone {
+  0% {
+    display: none;
+    opacity: 0;
+  }
+
+  1% {
+    display: block;
+    opacity: 0;
+  }
+
+  100% {
+    display: block;
+    opacity: 1;
+  }
+}
+
+@-o-keyframes fadeInFromNone {
+  0% {
+    display: none;
+    opacity: 0;
+  }
+
+  1% {
+    display: block;
+    opacity: 0;
+  }
+
+  100% {
+    display: block;
+    opacity: 1;
+  }
+}
+
+@keyframes fadeInFromNone {
+  0% {
+    display: none;
+    opacity: 0;
+  }
+
+  1% {
+    display: block;
+    opacity: 0;
+  }
+
+  100% {
+    display: block;
+    opacity: 1;
+  }
+}
+</style>
 <style lang="scss" scoped>
 .unselect {
+  color: #fff;
   user-select: none;
 }
 .select_menu {
