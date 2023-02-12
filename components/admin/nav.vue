0fa6449435cf75993a92a1d61509fa0ad2c342a1
DeadlyLemonate <pongpop.suw@gmail.com>
2023-02-12 17:01:09 +0700

    admin login front

---
+++ components/admin/nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav_wrapper">
+    <div v-for="(item, index) in menu" :key="index" :class="item.css">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        { name: 'home', css: 'menu' },
+        { name: 'about', css: 'menu' },
+        { name: 'services', css: 'menu' },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.nav_wrapper {
+  width: 100%;
+  height: 64px;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .menu {
+    color: #fff;
+    padding: 18px 16px;
+    width: 100px;
+    text-align: center;
+  }
+}
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="bg">
+    <v-card class="cardlog">
+      <div class="content">
+        <div>Log-in</div>
+      </div>
+      <div>
+        <div>
+          <v-text-field label="Username" v-model="username"></v-text-field>
+        </div>
+      </div>
+      <div style="margin-top: 12px">
+        <div>
+          <v-text-field label="Password" v-model="password"></v-text-field>
+        </div>
+      </div>
+      <div style="display: flex; justify-content: end">
+        <v-btn color="#014C56" small
+          ><div style="color: #fff" @click="login()">Login</div></v-btn
+        >
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import Nav from '~/components/admin/nav'
+export default {
+  layout: 'blank',
+  components: {
+    Nav,
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  mounted() {
+    console.log('session', sessionStorage.getItem('user'))
+  },
+  methods: {
+    login() {
+      let obj = {
+        username: this.username,
+        password: this.password,
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bg {
+  width: 100vw;
+  height: 100vh;
+  background: url('/bglog.jpg') no-repeat;
+  background-size: cover;
+  padding: 24px;
+  .cardlog {
+    width: 360px;
+    padding: 12px;
+    .content {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+}
+</style>
